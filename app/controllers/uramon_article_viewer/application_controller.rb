e9718709db111688bc4584a8506eb4893292dfde
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-15 18:10:52 +0900

    Move setup_connection to ApplicationController

--- app/controllers/uramon_article_viewer/application_controller.rb
+++ app/controllers/uramon_article_viewer/application_controller.rb
@@ -1,4 +1,19 @@
+require "faraday"
+
 module UramonArticleViewer
   class ApplicationController < ActionController::Base
+    BASE_URI = "http://localhost:3000/"
+
+    before_filter :setup_connection
+
+    private
+
+    def setup_connection
+      @connection = Faraday::Connection.new(:url => BASE_URI) do |builder|
+        builder.use Faraday::Request::UrlEncoded
+        builder.use Faraday::Response::Logger
+        builder.use Faraday::Adapter::NetHttp
+      end
+    end
   end
 end
--- app/controllers/uramon_article_viewer/articles_controller.rb
+++ app/controllers/uramon_article_viewer/articles_controller.rb
@@ -1,12 +1,7 @@
 require_dependency "uramon_article_viewer/application_controller"
-require "faraday"
 
 module UramonArticleViewer
-  class ArticlesController < ::ApplicationController
-    BASE_URI = "http://localhost:3000/"
-
-    before_filter :setup_connection
-
+  class ArticlesController < ApplicationController
     def index
       response = @connection.get("/admin/articles.json")
       @articles = JSON.parse(response.body)
@@ -15,16 +10,6 @@ module UramonArticleViewer
     def show
       response = @connection.get("/admin/articles/1.json")
       @article = JSON.parse(response.body)
-    end
-
-    private
-
-    def setup_connection
-      @connection = Faraday::Connection.new(:url => BASE_URI) do |builder|
-        builder.use Faraday::Request::UrlEncoded
-        builder.use Faraday::Response::Logger
-        builder.use Faraday::Adapter::NetHttp
-      end
     end
   end
 end
